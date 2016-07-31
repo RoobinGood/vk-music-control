12123d527e97cec0e6413f27941543bd40031094
roobingood <a.robingood@gmail.com>
2016-07-31 15:51:35 +0300

    open current or new vk tab on popup button click

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,17 @@
 require([
 	'helpers',
-	'anticipant',
-	'jquery',
-], function(Helpers, Anticipant, $) {
+], function(Helpers) {
 
-	// INIT APP
+	// redirect to audible vk tab or vk tab with player or to new vk tab
+	Helpers.getCurrentTab(function(tabId) {
+		if (tabId) {
+			console.log('redirect to', tabId);
+			chrome.tabs.update(tabId, {selected: true});
+		} else {
+			chrome.tabs.create({
+				url: 'https://new.vk.com/'
+			});
+		}
+	});
 
 });
